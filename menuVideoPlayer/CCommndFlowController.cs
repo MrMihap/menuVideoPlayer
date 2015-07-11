0434c10f46261fdadadf285575a1f0d5f1c0ccbf
MrMihap <MrMihap@yandex.ru>
2015-07-11 14:23:14 +0300

    player Window & commandController

---
+++ menuVideoPlayer/CCommndFlowController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menuVideoPlayer
+{
+  class CCommndsFlowController
+  {
+    public MainWindow menuWindow = null;
+    public PlayerWindow playerWindow = null;
+
+
+    void SendPlayID(int ID)
+    {
+      if (playerWindow != null) playerWindow.RecievePlayID(ID);
+    }
+  }
+}
---
+++ menuVideoPlayer/PlayerWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace menuVideoPlayer
+{
+  /// <summary>
+  /// Логика взаимодействия для PlayerWindow.xaml
+  /// </summary>
+  public partial class PlayerWindow : Window
+  {
+    private List<int> playList = new List<int>();
+    private int currentVideoID
+    {
+      set
+      {
+        throw new NotImplementedException("Change CurrentVideo");
+      }
+    }
+    public PlayerWindow()
+    {
+      InitializeComponent();
+    }
+    public void InitplayList()
+    { 
+
+    }
+    public void RecievePlayID(int ID)
+    {
+    }
+  }
+}
