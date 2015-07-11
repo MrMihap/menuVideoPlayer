f531aaa3bcff46c549577c8cc740a95c39623650
inthy <shtern94@gmail.com>
2015-07-11 15:45:53 +0300

    работает, надо доделать event

--- menuVideoPlayer/PlayerWindow.xaml.cs
+++ menuVideoPlayer/PlayerWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Core;
 
 namespace menuVideoPlayer
 {
@@ -19,24 +20,59 @@ namespace menuVideoPlayer
   /// </summary>
   public partial class PlayerWindow : Window
   {
-    private List<int> playList = new List<int>();
+    private bool mediaPlayerIsPlaying = false;
+    private int _currentVideoId=-1;
     private int currentVideoID
     {
       set
       {
-        throw new NotImplementedException("Change CurrentVideo");
+        mePlayer.Source = new Uri(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + "/"+Core.CMenuContainer.videoCollection.Where(x => x.ID == value).FirstOrDefault().VideoPath);
+        mediaPlayerIsPlaying = true;
+        //mePlayer.Play();
       }
     }
     public PlayerWindow()
     {
       InitializeComponent();
+
+      mePlayer.SourceUpdated += mePlayer_SourceUpdated;
+    
+      InitplayList();
     }
     public void InitplayList()
-    { 
-
+    {
+      Core.CMenuContainer.LoadFromConfig();
+      currentVideoID = 0;
     }
+
+    private void mePlayer_SourceUpdated(object e, DataTransferEventArgs args)
+    {
+      mediaPlayerIsPlaying = true;
+      mePlayer.Play();
+    }
+
+
+    private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+    {
+      e.CanExecute = (mePlayer != null) && (mePlayer.Source != null);
+    }
+
+    private void Play_Executed(object sender, ExecutedRoutedEventArgs e)
+    {
+
+      mePlayer.Play();
+      mediaPlayerIsPlaying = true;
+    }
+
+    //Продолжение после окончания
+    private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+    {
+      mePlayer.Position = TimeSpan.FromMilliseconds(1);
+    }
+
     public void RecievePlayID(int ID)
     {
+      currentVideoID = ID;
     }
   }
 }
