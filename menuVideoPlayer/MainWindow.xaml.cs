e63e4705f5f1cd9c7c7f4af7cabd9912edc1d378
Mikhail <mihap@fnbic.ru>
2015-07-13 11:40:36 +0300

    стилизованные кнопки

--- menuVideoPlayer/MainWindow.xaml.cs
+++ menuVideoPlayer/MainWindow.xaml.cs
@@ -18,53 +18,65 @@ using Core;
 using menuVideoPlayer.controls;
 namespace menuVideoPlayer
 {
-  /// <summary>
-  /// Логика взаимодействия для MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    //private CCommndsFlowController commandsController ;//= new CCommndsFlowController();
-    private PlayerWindow playerWindow;
-    public MainWindow()
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      CMenuContainer.LoadFromConfig();
-      InitializeComponent();
+        private bool IsClosed = false;
+        private PlayerWindow playerWindow;
+        public MainWindow()
+        {
+            CMenuContainer.LoadFromConfig();
+            InitializeComponent();
 
 
-      //this.Topmost = true;
-      this.WindowStartupLocation = System.Windows.WindowStartupLocation.Manual;
+            //this.Topmost = true;
+            this.WindowStartupLocation = System.Windows.WindowStartupLocation.Manual;
 
-      // создаем
-      playerWindow = new PlayerWindow();
-      if (Screen.AllScreens.Count() > 1)
-      {
-        Screen s1 = Screen.AllScreens[1];
-        //Screen s2 = Screen.AllScreens[1];
-        System.Drawing.Rectangle r1 = s1.WorkingArea;
-        //System.Drawing.Rectangle r2 = s2.WorkingArea;
-        this.Left = r1.Left;
-        this.Top = r1.Top;
+            // создаем
+            playerWindow = new PlayerWindow();
+            if (Screen.AllScreens.Count() > 1)
+            {
+                Screen s1 = Screen.AllScreens[1];
+                //Screen s2 = Screen.AllScreens[1];
+                System.Drawing.Rectangle r1 = s1.WorkingArea;
+                //System.Drawing.Rectangle r2 = s2.WorkingArea;
+                this.Left = r1.Left;
+                this.Top = r1.Top;
 
-        //playerWindow.Top = r2.Top;
-        //playerWindow.Left = r2.Left;
-      }
-      CCommndsFlowController.playerWindow = playerWindow;
+                //playerWindow.Top = r2.Top;
+                //playerWindow.Left = r2.Left;
+            }
+            CCommndsFlowController.playerWindow = playerWindow;
+            this.Closed += MainWindow_Closed;
+            playerWindow.Closed += playerWindow_Closed;
+            foreach (Core.MenuItem menuItem in CMenuContainer.videoCollection)
+            {
+                menuItemsListBox.Items.Add(new MenuItemControl(menuItem));
+            }
+        }
 
-      foreach (Core.MenuItem menuItem in CMenuContainer.videoCollection)
-      {
-        menuItemsListBox.Items.Add(new MenuItemControl(menuItem));
-      }
+        void MainWindow_Closed(object sender, EventArgs e)
+        {
+            IsClosed = true;
+            playerWindow.Close();
+        }
+
+        void playerWindow_Closed(object sender, EventArgs e)
+        {
+            if (!IsClosed) this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Maximized;
+            playerWindow.Show();
+        }
+
+        private void OnManipulationBoundaryFeedback(object sender, ManipulationBoundaryFeedbackEventArgs e)
+        {
+            e.Handled = true;
+        }
     }
-
-    private void Window_Loaded(object sender, RoutedEventArgs e)
-    {
-      this.WindowState = System.Windows.WindowState.Maximized;
-      playerWindow.Show();
-    }
-
-    private void OnManipulationBoundaryFeedback(object sender, ManipulationBoundaryFeedbackEventArgs e)
-    {
-      e.Handled = true;
-    }
-  }
 }
